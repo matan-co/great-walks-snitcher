c7722c53e9b2c454cc1a7d8968f278e01e905373
Matan <matanc@wix.com>
2022-08-20 02:26:37 +0300

    adding cron job

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var fs = require('fs');
+const client = require('twilio')(accountSid, authToken);
 
 function keplerLuxmore() {
     const file = fs.readFileSync('./data.txt', 'utf8');
     const dates = file.match(/Luxmore Hut is open on \d{2}\/\d{2}\/\d{4}. Total available is \d{1,2}/gm);
     const filtered = dates.map(x => x.split('Luxmore Hut is open on ')[1].split('. Total available is ')).filter(x => x[1] !== "0");
-    console.log(filtered);
+    return filtered;
 }
 
 async function main() {
-    keplerLuxmore();
+    const data = keplerLuxmore();
+    sendDataToTwilio(data)
+}
+
+function sendDataToTwilio(data) {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    client.messages.create({
+        body: data,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: process.env.MY_PHONE_NUMBER
+    });
 }
 
 main();
