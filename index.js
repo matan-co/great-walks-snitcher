538faaf067c196c3a7619bcc4de7a01d0d6f201a
Matan Cohen <33671703+matan-co@users.noreply.github.com>
2022-08-20 03:13:29 +0300

    adding cron job and sending sms via twillo account (#1)

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var fs = require('fs');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 function keplerLuxmore() {
     const file = fs.readFileSync('./data.txt', 'utf8');
     const dates = file.match(/Luxmore Hut is open on \d{2}\/\d{2}\/\d{4}. Total available is \d{1,2}/gm);
     const filtered = dates.map(x => x.split('Luxmore Hut is open on ')[1].split('. Total available is ')).filter(x => x[1] !== "0");
     console.log(filtered);
+    return filtered;
 }
 
 async function main() {
-    keplerLuxmore();
+    const data = keplerLuxmore();
+    await sendDataToTwilio(data)
+}
+
+async function sendDataToTwilio(data) {
+    client.messages.create({
+        body: JSON.stringify(data),
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: process.env.MY_PHONE_NUMBER
+    });
 }
 
 main();
